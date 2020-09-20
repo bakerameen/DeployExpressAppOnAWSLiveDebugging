9ceb8440e978d7360b4c4804cfcec9a4b524529b
unknown <baqer@baqqer-NB.aas.com.sa>
2020-09-20 22:36:47 +0400

    create db connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require('express');
-
+const mongoose = require("mongoose");
 var app = express()
+
+const dbPath = "mongodb+srv://baqer:iggy5R1y8urUhxts@cluster0-2wlh3.mongodb.net/Naseej-letter-competetion?retryWrites=true&w=majority";
+mongoose
+  .connect(dbPath, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connected!'))
+  .catch(err => {
+    console.log('error DB not Connected');
+  });
 
 app.get('/', (req, res) => {
 res.send('hello AWS');
